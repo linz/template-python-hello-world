34b8aa16d6c695f076daf34bcedf1038765d39f8
Daniel Silk <dsilk@linz.govt.nz>
2019-10-14 22:09:06 +1300

    feat: add function that takes a name, returns a greeting

---
+++ salutation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+It's pretty simple really. Hello. World.
+"""
+
+
+def hello(name: str = "World") -> str:
+    """Provide a name, return a greeting."""
+    return f"Hello, {name}!"
