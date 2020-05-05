c4741e98973c06e51f8c1880ee8fca66dd4b4b61
Daniel Silk <dsilk@linz.govt.nz>
2020-05-05 15:23:05 +1200

    test: fix assert statements that always evaluated to True

--- test_salutation.py
+++ test_salutation.py
@@ -7,14 +7,14 @@ from salutation import hello
 
 def test_hello_without_name():
     """Test with no parameter."""
-    assert "Hello, World!", hello()
+    assert hello() == "Hello, World!"
 
 
 def test_hello_with_sample_name():
     """Test with a name supplied."""
-    assert "Hello, Alice!", hello("Alice")
+    assert hello("Alice") == "Hello, Alice!"
 
 
 def test_hello_with_macronated_name():
     """Test with a macronated name supplied."""
-    assert "Hello, Ātaahua!", hello("Ātaahua")
+    assert hello("Ātaahua") == "Hello, Ātaahua!"
