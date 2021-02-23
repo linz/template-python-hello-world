8ff93e767b68c73e411802d4883a3d881c627cdf
Victor Engmark <vengmark@linz.govt.nz>
2021-02-23 13:36:12 +1300

    Add mypy pre-commit hook (#118)
    * feat: Run all pre-commit hooks in CI
    
    Rather than running a specific subset of linters, this keeps the results
    in CI in sync with the results when committing.
    
    * feat: Check type annotations using mypy
    
    See <https://mypy.readthedocs.io/en/stable/config_file.html> for
    documentation on the stricter options in setup.cfg.
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- test_salutation.py
+++ test_salutation.py
@@ -5,16 +5,16 @@ Tests for the hello() function.
 from salutation import hello
 
 
-def test_hello_without_name():
+def test_hello_without_name() -> None:
     """Test with no parameter."""
     assert hello() == "Hello, World!"
 
 
-def test_hello_with_sample_name():
+def test_hello_with_sample_name() -> None:
     """Test with a name supplied."""
     assert hello("Alice") == "Hello, Alice!"
 
 
-def test_hello_with_macronated_name():
+def test_hello_with_macronated_name() -> None:
     """Test with a macronated name supplied."""
     assert hello("Ātaahua") == "Hello, Ātaahua!"
