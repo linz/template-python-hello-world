1a8c352528c0b9862bda4a86232b8b0ffaeb3567
Victor Engmark <vengmark@linz.govt.nz>
2021-10-20 14:57:00 +1300

    feat: Add simple CLI example

--- src/salutation.py
+++ src/salutation.py
@@ -1,2 +1,16 @@
+from typer import Option, Typer, echo
+
+app = Typer()
+
+
 def hello(name: str = "World") -> str:
     return f"Hello, {name}!"
+
+
+@app.command()
+def main(name: str = Option(...)) -> None:
+    echo(hello(name))
+
+
+if __name__ == "__main__":
+    app()
